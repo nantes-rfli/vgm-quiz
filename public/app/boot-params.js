4fb20d259325e32e0eba63ded9cc306a9cbd1ccc
nanto <nantes-rfli@users.noreply.github.com>
2025-09-11 12:39:09 +0900

    Streamline early i18n initialization

--- public/app/boot-params.js
+++ public/app/boot-params.js
@@ -3,12 +3,3 @@
 const __SEARCH_PARAMS__ = new URLSearchParams(location.search);
 const __IS_TEST_MODE__ = __SEARCH_PARAMS__.get('test') === '1';
 
-
-// Early-set <html lang> for E2E and a11y (non-blocking; i18n.mjs will normalize later)
-try {
-  const __LANG__ = __SEARCH_PARAMS__.get('lang');
-  if (__LANG__) {
-    document.documentElement.setAttribute('lang', __LANG__);
-  }
-} catch {}
-
